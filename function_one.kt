e40cba6f74caa00c6c091f7fb7b54b5d1ed5a2c4
luis.miranda <luizgcmiranda@gmail.com>
2024-02-14 19:48:14 -0300

    Add new functions. Utility foreach.

--- function_one.kt
+++ function_one.kt
@@ -11,12 +11,26 @@ fun printColored(message: String, color: String) {
     println("$ANSI_RESET[$color$message$ANSI_RESET]")
 }
 
+fun printAllWithPrefix(vararg messages: String, prefix: String) {
+    for (ms in messages)
+        println(prefix + ms)
+}
+
+fun printTest1(vararg messages: String, prefix: String) {
+    messages.forEach { println(prefix + it)}
+}
+
+
 fun main() {
 
-    printGitHub("Hello, world!")
+    printGitHub("Olá, bem-vindo!")
 
     printGitHubFormatted("O valor de pi é %.2f", Math.PI)
 
-    printColored(" This is red text", "red")
+    printColored(" Esse texto é vermelho", "red")
 
+    printAllWithPrefix("Esse é um dos meus primeiros aprendizados com a linguagem", "Está sendo muito bom", "Estou me divertindo",
+    prefix = "[ALERTA] - ")
+
+    printTest1("Teste A", "Teste B", "Teste C", prefix = "[TESTE] - ")
 }
