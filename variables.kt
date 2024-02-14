4f867f7baeec5efb74923a34c6941439d42a6495
luis.miranda <luizgcmiranda@gmail.com>
2024-02-14 20:35:42 -0300

    Apply null safety in variables.kt

--- variables.kt
+++ variables.kt
@@ -34,4 +34,36 @@ fun main() {
     e = 20 // Adicionado com sucesso por conta do 'e' ser um var.
     println(e)
 
+    ///////////////////////////////////////////////////////
+
+    // Null Safety (?).
+
+    var neverNull: String = "Isso não pode ser nulo"
+    // neverNull = null
+    // Deu erro pois faltou o ? após a tipagem para classificar a variável como possível nulo.
+
+    var nullable: String? = "Você pode manter um nulo aqui"
+    nullable = null
+
+    // var inferredNonNull = "O compilador assume não nulo" Vai dar erro se não passar a tipagem e ?.
+    var inferredNonNull: String? = "O compilador assume não nulo"
+    inferredNonNull = null
+
+    fun strLength(str: String?): Int {
+        return str?.length ?: 0
+    }
+
+    fun describeString(maybeString: String?): String {
+        if (maybeString != null && maybeString.length > 0) {
+            return "A String possui um tamanho de: ${maybeString.length}."
+        } else {
+            return "A string está nula ou vazia."
+        }
+    }
+
+    println(strLength(neverNull))
+    println(strLength(nullable))
+
+    println(describeString(null))
+    println(describeString("Esse é um teste para validar a função describreString"))
 }
